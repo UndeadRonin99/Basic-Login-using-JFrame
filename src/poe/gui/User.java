1d3c1afab326b892c60008ef315f90464e0b652b
UndeadRonin99 <104002258+UndeadRonin99@users.noreply.github.com>
2022-05-11 16:09:31 +0200

    Add files via upload

---
+++ src/poe/gui/User.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poe.gui;
+
+/**
+ *
+ * @author Mike
+ */
+public class User {
+    public String username;
+    public String password;
+    public String firstName;
+    public String surname;
+    
+    
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String inUsername) {
+        this.username = inUsername;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String inPassword) {
+        this.password = inPassword; 
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String inFirstName) {
+        this.firstName = inFirstName; 
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String inSurname) {
+        this.surname = inSurname; 
+    }
+    
+    
+    
+    
+    
+}
